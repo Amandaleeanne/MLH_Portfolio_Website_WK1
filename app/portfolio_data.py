30be76e1ea26e7f2047b3c16d41ff160029de16c
Amandaleeanne Schock <145935534+Amandaleeanne@users.noreply.github.com>
2026-06-17 14:32:04 -0700

    Fully filled out the layout of the website
    Made sure to use Jinja template and added a travel map using Plotly, that was a beast. Will be updating with actual data later.

--- app/portfolio_data.py
+++ app/portfolio_data.py
@@ -4,16 +4,16 @@ ABOUT_TEXT = "Hello! I'm an MLH Fellow passionate about building software."
 
 WORK_EXPERIENCES = [
     {
-        "title": "Software Engineering Intern",
+        "title": "Example 1",
         "subtitle": "Example Company",
         "date": "Summer 2025",
-        "description": "Built web app features with a cross-functional team.",
+        "description": "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
     {
-        "title": "Teaching Assistant",
+        "title": "Example 2",
         "subtitle": "University CS Department",
         "date": "2024 - Present",
-        "description": "Helped students in intro programming courses.",
+        "description": "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
 ]
 
@@ -35,7 +35,7 @@ EDUCATION = [
 PERSONAL_PROJECTS = [
     {
         "title": "Example 1",
-        "description": "Example description",
+        "description": "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         "link": "https://example.com",
         "image": "img/logo.jpg",
     },
@@ -52,13 +52,26 @@ SKILLS = [
 
 HOBBIES = [
     {
-        "title": "Photography",
-        "description": "Landscapes and cityscapes on weekend walks.",
+        "title": "Example Hobby 1",
+        "description": "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         "image": "img/logo.jpg",
     },
     {
-        "title": "Reading",
-        "description": "Science fiction and tech blogs.",
+        "title": "Example Hobby 2",
+        "description": "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         "image": "img/logo.jpg",
     },
+]
+
+PLACES = [
+    {"name": "Los Angeles, CA", "lat": 34.0522, "lon": -118.2437, "left": 16.8, "top": 72.5, "note": "Family visit to my uncle."},
+    {"name": "Kemah, TX", "lat": 29.5702, "lon": -95.0203, "left": 57.5, "top": 77.5, "note": "First offline meetup with friends."},
+    {"name": "Savage River Trail, Denali NP", "lat": 63.3850, "lon": -148.5953, "left": 17.0, "top": 87.0, "note": "Hiked Savage Trail in Denali National Park."},
+    {"name": "Paradise, Mt. Rainier", "lat": 46.7843, "lon": -121.7414, "left": 17.5, "top": 51.5, "note": "Hiked Paradise Trail at Mt. Rainier."},
+    {"name": "North Olympic NP", "lat": 48.1193, "lon": -123.4307, "left": 14.2, "top": 47.5, "note": "Hiked multiple trails in northern Olympic National Park."},
+    {"name": "South Olympic NP", "lat": 47.6405, "lon": -123.6044, "left": 14.0, "top": 60.5, "note": "Hiked multiple trails in southern Olympic National Park."},
+    {"name": "Ape Cave Lower Trail", "lat": 46.2000, "lon": -122.1700, "left": 18.8, "top": 60.0, "note": "Hiked the Ape Cave lower trail."},
+    {"name": "SeaWorld San Diego", "lat": 32.7640, "lon": -117.2260, "left": 18.2, "top": 79.0, "note": "Visited SeaWorld as a kid."},
+    {"name": "Concord, NH", "lat": 43.2081, "lon": -71.5376, "left": 93.5, "top": 42.5, "note": "New Hampshire glass conference with my mother."},
+    {"name": "Oregon Coast", "lat": 45.8919, "lon": -123.9615, "left": 12.5, "top": 65.0, "note": "Family trip to the Oregon beach in the rain."},
 ]
