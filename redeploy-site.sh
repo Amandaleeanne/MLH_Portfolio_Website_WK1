2c5a7df50cd03d31a6bb06dd64031f35b848ebc6
Amandaleeanne Schock <145935534+Amandaleeanne@users.noreply.github.com>
2026-07-06 11:46:52 -0700

    update for systemctl

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-tmux kill-server && cd /MLH_Portfolio_website_WK2 && git fetch && git reset origin/HEAD --hard && python -m venv python3-virtualenv && source python3-virtualenv/bin/activate && tmux new-session -d -s flask 'flask run; bash'
+cd /MLH_Portfolio_website_WK2 && git fetch && git reset origin/HEAD --hard && python -m venv python3-virtualenv && source python3-virtualenv/bin/activate && systemctl daemon-reload && systemctl restart myportfolio
+
+
+
+
+
 
 
 
