5ae6e247893811f2ad5e0bab6b8329ef71824117
Amandaleeanne Schock <145935534+Amandaleeanne@users.noreply.github.com>
2026-07-13 13:18:06 -0700

    streamlined
    Removed virtual environment creation and added pip install for requirements.

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~/MLH_Portfolio_Website_WK2 && git fetch && git reset origin/HEAD --hard && python -m venv python3-virtualenv && source python3-virtualenv/bin/activate && systemctl daemon-reload && systemctl restart myportfolio
+cd ~/MLH_Portfolio_Website_WK2 && git fetch && git reset origin/HEAD --hard && source python3-virtualenv/bin/activate && pip install -r requirements.txt && systemctl daemon-reload && systemctl restart myportfolio
 
 
 
