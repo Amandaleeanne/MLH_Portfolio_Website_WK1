43e8a9045ff0f9913fc34782cfb9746652cbe4af
Amandaleeanne Schock <145935534+Amandaleeanne@users.noreply.github.com>
2026-07-24 10:37:31 -0700

    updated redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-cd ~/MLH_Portfolio_Website_WK2 && git fetch && git reset origin/HEAD --hard && source python3-virtualenv/bin/activate && pip install -r requirements.txt && systemctl daemon-reload && systemctl restart myportfolio
-
-
-
-
-
-
-
-
-
-
-
+cd ~/MLH_Portfolio_Website_WK2 && git fetch && git reset origin/HEAD --hard && docker compose -f docker-compose.prod.yml down && docker compose -f docker-compose.yml up -d --build
 
