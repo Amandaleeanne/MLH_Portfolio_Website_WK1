193666de0df886ed52388f0b33edbdffda42085a
Amandaleeanne Schock <145935534+Amandaleeanne@users.noreply.github.com>
2026-07-27 15:05:21 -0700

    updated redeploy-site.sh and timeline page

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd ~/MLH_Portfolio_Website_WK2 && git fetch && git reset origin/HEAD --hard && docker compose -f docker-compose.prod.yml down && docker compose -f docker-compose.yml up -d --build
+cd ~/MLH_Portfolio_Website_WK2 && git fetch && git reset origin/HEAD --hard && docker compose -f docker-compose.prod.yml down && docker compose -f docker-compose.prod.yml up -d --build
 
